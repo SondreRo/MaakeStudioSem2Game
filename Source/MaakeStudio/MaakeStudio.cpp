b6fb09df445550e5c1e0832a21c7b44142a855d1
Sondre R <soroensg@gmail.com>
2023-03-22 12:19:20 +0100

    Created Unreal Project

---
+++ Source/MaakeStudio/MaakeStudio.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MaakeStudio.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MaakeStudio, "MaakeStudio" );
---
+++ Source/MaakeStudio/MaakeStudioGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MaakeStudioGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAAKESTUDIO_API AMaakeStudioGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
